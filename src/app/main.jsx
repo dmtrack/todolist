6c2297ab9b3a1d756fc533fa8d6326ea67ee8d35
dmtrack <rightrack888@gmail.com>
2022-11-19 09:58:44 +0600

    add: react-router, main page file

---
+++ src/app/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <>
+      <div className="main-content">
+        <h1 className="heading">my todoList</h1>
+      </div>
+    </>
+  );
+}
+export { MainPage };
